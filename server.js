dd267859f45652abef80a156118177fcbe84a183
Jason Albalah <Jason Albalah>
2016-09-05 06:58:19 -0400

    5bth

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.get('/', function (req, res) {
   }
 });
 
-/*app.get('/pagecount', function (req, res) {
+app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
@@ -94,7 +94,7 @@ app.get('/', function (req, res) {
   } else {
     res.send('{ pageCount: -1 }');
   }
-});*/
+});
 
 // error handling
 app.use(function(err, req, res, next){
