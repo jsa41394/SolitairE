40f67128c4029c4438de4f34fa471681e3108eff
Jason Albalah <Jason Albalah>
2016-09-17 13:42:44 -0400

    2016-09-17T13:42:44-04:00

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.get('/', function (req, res) {
         //test = d;
         
         db.collection('counts').find().toArray(function (err, results) {
-            test = results;
+            test = results[0].rand;
             // send HTML file populated with quotes here
         })
         
