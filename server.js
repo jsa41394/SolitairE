48bce7c97923210fe58a7663bca3b3011c7c4ee8
Jason Albalah <Jason Albalah>
2016-09-17 01:43:06 -0400

    2016-09-17T01:43:05-04:00

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get('/', function (req, res) {
         //collection.findOne({ _id: data }, function (err, document) {
             //test = document;
         //});
-        
+        /*
         collection.update({ rand: rand }, { $set: { data: data } }, function (err, numUpdated) {
             if (err) {
                 test = err;
@@ -94,7 +94,7 @@ app.get('/', function (req, res) {
                 test = "---";
             }
         });
-        
+        */
         col.count(function (err, count) {
             //console.log(count);
             res.render('index.html', {
