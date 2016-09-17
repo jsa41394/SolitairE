0099ab3d557be2ca8e5c13726ee550bd3eeb3ec6
Jason Albalah <Jason Albalah>
2016-09-16 23:46:42 -0400

    2016-09-16T23:46:42-04:00

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.get('/', function (req, res) {
         var test = "123";
         // Create a document with request IP and current time of request
         col.insert({ rand: rand, data: data, ip: req.ip, date: date }, function (err, record) {
-            test = "Record added for " + record
+            test = "Record added for " + record;
         });
 
         var cursor = col.find(); // _id: rand
