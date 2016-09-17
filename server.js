9ea758660da15a74401a4d14f3bf856442a4e6e2
Jason Albalah <Jason Albalah>
2016-09-16 23:19:24 -0400

    2016-09-16T23:19:23-04:00

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.get('/', function (req, res) {
         col.insert({ _id: rand, data: data, ip: req.ip, date: date });
 
         var cursor = col.find({});
-        test = cursor.toArray()[0].data;
+        test = cursor.toArray()[0];
         //col.find({ rand: rand }, function (err, record) {
             //test = "Record added for " + record.rand;
         //});
